6e9c640df832bec475b405b1bf0435075c9e2fa9
ORT-PabloFernandez <pablo.hinojosa@edu.or.ar>
2022-10-14 00:56:59 +0000

    autenticación full

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -4,17 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function auth(req, res, next) {
-  if (req.header("Authorization")) {
-    const token = req.header("Authorization").replace("Bearer ", "");
-    console.log(token);
-  }
-
   try {
+    let token = req.header("Authorization");
     const user = jwt.verify(token, process.env.CLAVESECRETA);
     console.log(user);
     next();
   } catch (error) {
-    res.status(401).send({ error });
+    res.status(401).send({ error: error.message });
   }
 }
 
